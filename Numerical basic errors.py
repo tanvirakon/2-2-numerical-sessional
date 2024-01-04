b55e0a4774e9fc197096e59f4a47c8e7e7358522
tanvirakon <akon.pt33@gmail.com>
2024-01-04 11:33:51 +0600

    test

---
+++ Numerical basic errors.py
@@ -0,0 +1,9 @@
+import math
+
+exact_value=math.pi
+
+appox=3.1416
+
+round_of_error=abs(exact_value-appox)
+# print(f"Round of error : {round_off_error}")
+print(f"round of error : {round_of_error}")
